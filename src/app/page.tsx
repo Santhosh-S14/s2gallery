29d47ea52c35e90bf7acf0ea509b2d2d04ee4097
Santhosh S <39198124+Santhosh-S14@users.noreply.github.com>
2024-05-19 19:29:12 -0400

    Database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
+/* eslint-disable @next/next/no-img-element */
 import Link from "next/link";
+import { db } from "~/server/db";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
   const mockUrls = [
     "https://utfs.io/f/dbad9f40-ea2d-497c-81e3-fb1d5aaa63ce-1x9zs6.png",
     "https://utfs.io/f/1c3c943a-bd95-41f3-ae1c-963c4fa4c6f9-1xbb9e.png",
@@ -15,8 +19,11 @@ export default function HomePage() {
   return (
     <main>
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
-          <div key={image.id} className="h-48 w-48">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
+        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+          <div key={image.id + "-" + index} className="h-48 w-48">
             <img
               src={image.url}
               alt="mock image"
