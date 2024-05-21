8f6c2db4f87f5d5ef5cd941b416fc05ffc123eb1
Santhosh S <39198124+Santhosh-S14@users.noreply.github.com>
2024-05-21 18:54:23 -0400

    connected users to images

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -30,6 +30,7 @@ export const ourFileRouter = {
       await db.insert(images).values({
         name: file.name,
         url: file.url,
+        userId: metadata.userId,
       });
  
       // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,12 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Link from "next/link";
 import { db } from "~/server/db";
+import { getMyImages } from "~/server/queries";
 
 export const dynamic = "force-dynamic";
 
 async function Images() {
-  const images = await db.query.images.findMany({
-    orderBy: (model, { desc }) => desc(model.id),
-  });
+  const images = await getMyImages();
   return (
     <div className="flex flex-wrap gap-4">
       {images.map((image) => (
