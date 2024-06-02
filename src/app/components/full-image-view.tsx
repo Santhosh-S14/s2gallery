4b70c00886bbad50bbe38035f7748a19997970d2
Santhosh S <39198124+Santhosh-S14@users.noreply.github.com>
2024-06-02 12:39:15 -0400

    fixed sizing and paddings for the modal

--- src/app/components/full-image-view.tsx
+++ src/app/components/full-image-view.tsx
@@ -4,5 +4,14 @@ export default async function FullPageImageView(
   props: Readonly<{ id: number }>,
 ) {
   const img = await getImageById(props.id);
-  return <img src={img.url} className="h-96 w-96" />;
+  return (
+    <div className="flex h-full w-full">
+      <div className="flex items-center justify-center object-contain">
+        <img src={img.url} className="object-contain" />
+      </div>
+      <div className="flex flex-col border-l">
+        <h1 className="text-2xl font-bold">{img.name}</h1>
+      </div>
+    </div>
+  );
 }
