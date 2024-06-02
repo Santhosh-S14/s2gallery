be89096f7f476069841b6cdf5d09fa3cb819492c
Santhosh S <39198124+Santhosh-S14@users.noreply.github.com>
2024-06-02 17:47:52 -0400

    added image info to the side

--- src/app/@modal/(.)img/[id]/modal.tsx
+++ src/app/@modal/(.)img/[id]/modal.tsx
@@ -21,7 +21,7 @@ export function Modal({ children }: { children: React.ReactNode }) {
   return createPortal(
     <dialog
       ref={dialogRef}
-      className="h-screen w-screen bg-zinc-900/50 text-white"
+      className="h-screen w-screen bg-black/80 text-white"
       onClose={onDismiss}
     >
       {children}
--- src/app/components/full-image-view.tsx
+++ src/app/components/full-image-view.tsx
@@ -1,16 +1,28 @@
+import { clerkClient } from "@clerk/nextjs/server";
 import { getImageById } from "~/server/queries";
 
 export default async function FullPageImageView(
   props: Readonly<{ id: number }>,
 ) {
   const img = await getImageById(props.id);
+  const uploaderInfo = await clerkClient.users.getUser(img.userId!);
   return (
-    <div className="flex h-full w-full">
+    <div className="flex h-full w-screen">
       <div className="flex items-center justify-center object-contain">
         <img src={img.url} className="object-contain" />
       </div>
-      <div className="flex flex-col border-l">
-        <h1 className="text-2xl font-bold">{img.name}</h1>
+      <div className="flex w-48 flex-col border-l">
+        <div className="border-b p-2 text-center text-lg font-bold">
+          {img.name}
+        </div>
+        <div className="flex flex-col p-2">
+          <span>Uploaded by: </span>
+          <span>{uploaderInfo.fullName}</span>
+        </div>
+        <div className="flex flex-col p-2">
+          <span>Created at: </span>
+          <span>{new Date(img.createdAt).toLocaleDateString()}</span>
+        </div>
       </div>
     </div>
   );
