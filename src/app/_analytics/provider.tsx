aa3cd91b3f86b6f0d2646bd4ca01cfd7bc592913
Santhosh S <39198124+Santhosh-S14@users.noreply.github.com>
2024-06-08 12:56:58 -0400

    using nextjs rewrites for reverse proxy & for ad blockers

--- src/app/_analytics/provider.tsx
+++ src/app/_analytics/provider.tsx
@@ -6,8 +6,8 @@ import { useEffect } from "react";
 
 if (typeof window !== "undefined") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-    person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+    api_host: "/ingest",
+    ui_host: "https://us.posthog.com",
   });
 }
 export function CSPostHogProvider({
